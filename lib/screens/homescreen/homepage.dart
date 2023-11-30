db36d873312da13c1e82b3b044e27890d2ba0884
kingdemon24 <virajbogati11@gmail.com>
2023-12-01 02:32:51 +0545

    code

--- lib/screens/homescreen/homepage.dart
+++ lib/screens/homescreen/homepage.dart
@@ -75,64 +75,100 @@ class MyHomePage extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Container(
-                      // color: Colors.amber,
-                      height: height*0.17,
-                      width: height*0.2,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Color(kAppBar)
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Image.asset(
-                              Assets.images.routine,
-                            ),
-                            SizedBox(height: kVerticalMargin,),
-                            ResponsiveText(
-                              kMySchedule,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                    GestureDetector(
+  onTap: () {
+    // Navigate to another screen when the container is pressed
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => RegisterScreen()),
+    );
+  },
+  child: Container(
+    height: height * 0.17,
+    width: height * 0.2,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Color(kAppBar),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Image.asset(
+            Assets.images.routine,
+          ),
+          SizedBox(height: kVerticalMargin),
+          ResponsiveText(
+            kMySchedule,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ],
+      ),
+    ),
+  ),
+),
                     SizedBox(width: kHorizontalMargin),
-                    Container(
-                      // color: Colors.amber,
-                      height: height*0.17,
-                      width: height*0.2,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Color(kAppBar)
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
+                    GestureDetector(
+  onTap: () {
+    // Navigate to another screen when the container is pressed
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => RegisterScreen()),
+    );
+  },
+  child: Container(
+    height: height * 0.17,
+    width: height * 0.2,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Color(kAppBar),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Image.asset(
+            Assets.images.mylessonplan,
+          ),
+          SizedBox(height: kVerticalMargin),
+          ResponsiveText(
+            kMyLessonPlan,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ],
+      ),
+    ),
+  ),
+),
+                    // Container(
+                    //   // color: Colors.amber,
+                    //   height: height*0.17,
+                    //   width: height*0.2,
+                    //   decoration: BoxDecoration(
+                    //     borderRadius: BorderRadius.circular(8),
+                    //     color: Color(kAppBar)
+                    //   ),
+                    //   child: Padding(
+                    //     padding: const EdgeInsets.all(8.0),
+                    //     child: Column(
                           
-                          children: [
-                            Image.asset(
-                              Assets.images.mylessonplan,
-                            ),
-                            SizedBox(height: kVerticalMargin,),
-                            ResponsiveText(
-                              kMyLessonPlan,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
+                    //       children: [
+                    //         Image.asset(
+                    //           Assets.images.mylessonplan,
+                    //         ),
+                    //         SizedBox(height: kVerticalMargin,),
+                    //         ResponsiveText(
+                    //           kMyLessonPlan,
+                    //           fontSize: 16,
+                    //           fontWeight: FontWeight.w600,
                               
-                            ),
-                          ],
-                        ),
-                      ),
-    //                   onPressed: (){
-    //                       Navigator.push(
-    // context,
-    // MaterialPageRoute(builder: (context) =>  RegisterScreen()),
-    //                     );},
-                    ),
+                    //         ),
+                    //       ],
+                    //     ),
+                    //   ),
+                    // ),
                     
                   ],
                 ),
@@ -143,31 +179,65 @@ class MyHomePage extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Container(
-                      // color: Colors.amber,
-                      height: height*0.17,
-                      width: height*0.2,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Color(kAppBar)
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Image.asset(
-                              Assets.images.myaibot,
-                            ),
-                            SizedBox(height: kVerticalMargin,),
-                            ResponsiveText(
-                              kMyAiBot,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                    GestureDetector(
+  onTap: () {
+    // Navigate to another screen when the container is pressed
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => RegisterScreen()),
+    );
+  },
+  child: Container(
+    height: height * 0.17,
+    width: height * 0.2,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Color(kAppBar),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Image.asset(
+            Assets.images.myaibot,
+          ),
+          SizedBox(height: kVerticalMargin),
+          ResponsiveText(
+            kMyAiBot,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ],
+      ),
+    ),
+  ),
+),
+
+                    // Container(
+                    //   // color: Colors.amber,
+                    //   height: height*0.17,
+                    //   width: height*0.2,
+                    //   decoration: BoxDecoration(
+                    //     borderRadius: BorderRadius.circular(8),
+                    //     color: Color(kAppBar)
+                    //   ),
+                    //   child: Padding(
+                    //     padding: const EdgeInsets.all(8.0),
+                    //     child: Column(
+                    //       children: [
+                    //         Image.asset(
+                    //           Assets.images.myaibot,
+                    //         ),
+                    //         SizedBox(height: kVerticalMargin,),
+                    //         ResponsiveText(
+                    //           kMyAiBot,
+                    //           fontSize: 16,
+                    //           fontWeight: FontWeight.w600,
+                    //         ),
+                    //       ],
+                    //     ),
+                    //   ),
+                    // ),
                     SizedBox(width: kHorizontalMargin),
                     // Container(
                     //   // color: Colors.amber,
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class login extends StatefulWidget {
   const login({super.key});
 
